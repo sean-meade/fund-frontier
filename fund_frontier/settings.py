c91abf273594a39c0beb940bbce9164600747e3d
Bogdan <bogdan.gugu93@gmail.com>
2024-01-18 08:46:40 +0000

    Made mgirations, add correct path for css and add basic styling

--- fund_frontier/settings.py
+++ fund_frontier/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-**5+#@p7j7aq#-2&tw@=z!!qbac=z#h#@=9ozagky4-0x=gb8r'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-qburn93-fundfrontier-zlme3vsnzam.ws-eu107.gitpod.io']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
