22fe3ebfd2d0231d65aa2c6eb6ddc69cf0332c21
Sean <seanpatmeade@gmail.com>
2024-01-18 21:08:47 +0000

    update urls because of view name change

--- npv/urls.py
+++ npv/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calculate_NPV_form, name='calculate-npv'),  # URL for the NPV calculation form
-    path('list-projects/', views.list_projects, name='list-projects'),
+    path('npv/', views.calculate_NPV_form, name='calculate-npv'),  # URL for the NPV calculation form
+    path('list_evaluations/', views.list_evaluations, name='list_evaluations'),
 ]
