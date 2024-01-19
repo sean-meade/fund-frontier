6672e997d0c508177a47879a9c566a340bbfa5b0
Redsskull <ivry91@gmail.com>
2024-01-19 01:13:11 +0100

    pushing brainstorm branch, DO NOT MERGE

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", 'H[D~_7`kWZ4ubABN!xns?he%tB%?(e}8t$F1)M!u[HTA#xQ?!Yq;~@{"[-+`$7b')   
--- npv/views.py
+++ npv/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, render, redirect
 from django.shortcuts import render, redirect
 from .forms import NPV_Form
 from .models import Project, Evaluation
@@ -8,20 +9,19 @@ def calculate_npv_form(request):
         # create a form instance and populate it with data from the request:
         form = NPV_Form(request.POST, extra=request.POST.get('cash_flow_year_count'))
         # check whether it's valid:
+        # create a form instance and populate it with data from the request:
+        form = NPV_Form(request.POST, extra=request.POST.get('cash_flow_year_count'))
+        # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # Create list for cash flows by adding initial investment
             cash_flows = [-(form.cleaned_data["initial_investment"])]
             # Loop through each cash flow and add to cash flow list
-            print("form.cleaned_data", form.cleaned_data)
-            for i in range(1, int(form.cleaned_data["cash_flow_year_count"]) + 1):
+            for i in range(1, int(form.cleaned_data["cash_flow_year_count"])):
                 cash_flows.append(form.cleaned_data["cash_flow_year_"+str(i)])
+            print("cash_flows", cash_flows)
             # save discount rate
             discount_rate = form.cleaned_data["discount_rate"]
-            
-            print("cash_flows", cash_flows)
-            # Calculate the npv
-            # npv = calculate_NPV(cash_flows, discount_rate)
             # render page with npv value
             evaluation = Evaluation.objects.create(
                 name = "evaluation_name",
@@ -38,9 +38,51 @@ def calculate_npv_form(request):
             project.save()
            
             return render(request, "npv/calculate-npv.html", {"form": form}) 
-        print("not valid")
     form = NPV_Form()       
     return render(request, "npv/calculate-npv.html", {"form": form})
+# def calculate_npv_form(request):
+
+
+
+
+
+
+# def calculate_NPV_form_2(request):
+#     if request.method == "POST":
+#         form = NPV_Form(request.POST, extra=request.POST.get('cash_flow_year_count', 0))
+#         if form.is_valid():
+#             # Fetch or create an Evaluation instance
+#             evaluation = Evaluation.objects.first()  # Fetch the first Evaluation instance
+#             if not evaluation:
+#                 # If there's no Evaluation instance, create one
+#                 evaluation = Evaluation.objects.create(discount_rate=form.cleaned_data["discount_rate"])
+
+#             # Get form data
+#             initial_investment = form.cleaned_data["initial_investment"]
+#             period = int(form.cleaned_data["cash_flow_year_count"])
+#             project_name = 'New Project'  # Set a project name or get it from form
+
+#             # Create a new Project instance
+#             project = Project.objects.create(
+#                 evaluation=evaluation,
+#                 name=project_name,
+#                 initial_investment=initial_investment,
+#                 period=period,
+#             )
+
+#             # Assuming you have a way to get annual net cash flows from the form
+#             for year in range(1, period + 1):
+#                 cash_flow = form.cleaned_data[f'cash_flow_year_{year}']
+#                 # Add the cash flow to the project
+#                 project.cash_flows.create(year=year, amount=cash_flow)
+
+#             project.save()  # Save the project instance to calculate and store NPV and other metrics automatically
+
+#             return redirect('list-projects')  # Redirect to the list of projects after successful form submission
+#     else:
+#         form = NPV_Form()
+
+#     return render(request, "npv/calculate-npv.html", {"form": form})
 # def calculate_npv_form(request):
 
 
