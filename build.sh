abf98084af17a07c5fad475072ebd3e7b1081ff1
Sean <seanpatmeade@gmail.com>
2024-01-22 02:57:14 +0000

    try this

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -o errexit
-sudo apt install libpq-dev python3-dev
 pip install -r requirements.txt
+sudo apt-get install libpq-dev
+pip install psycopg2==2.9.9
 python manage.py collectstatic --noinput
 python manage.py makemigrations && python manage.py migrate
 python manage.py createsuperuser --no-input
